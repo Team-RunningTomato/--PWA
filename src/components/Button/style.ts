cc92fb8c726f4cc814123ccf040e4c31eb30e709
gjaegyun <jaegyungim06@gmail.com>
2024-11-14 20:58:44 +0900

    feat: button storybook 세팅

--- src/components/Button/style.ts
+++ src/components/Button/style.ts
@@ -1,6 +1,6 @@
 import styled from '@emotion/styled';
 
-export const Wrapper = styled.div`
+export const Wrapper = styled.button`
   display: flex;
   width: 20.5rem;
   padding: 0.875rem 9.375rem;
@@ -8,6 +8,11 @@ export const Wrapper = styled.div`
   align-items: center;
   border-radius: 0.5rem;
   background: #1c1c1c; //추후 theme변경 시에 theme로 변경
+  transition: background 0.3s ease;
+
+  &:hover {
+    background: #383838;
+  }
 `;
 
 export const Text = styled.p`
