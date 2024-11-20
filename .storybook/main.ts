e586ab0a120f53885855c550ec899fda2fdf51bd
gjaegyun <jaegyungim06@gmail.com>
2024-11-20 17:11:43 +0900

    feat: 필요없는 경로 삭제

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,7 +2,7 @@ import type { StorybookConfig } from '@storybook/nextjs';
 import path from 'path';
 
 const config: StorybookConfig = {
-  stories: ['../src/**/*.stories.tsx', '**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  stories: ['../src/**/*.stories.tsx'],
   addons: [
     '@storybook/addon-onboarding',
     '@storybook/addon-links',
