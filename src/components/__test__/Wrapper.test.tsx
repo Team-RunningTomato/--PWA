33718354f108e828cab4b1cdf92ea5705b3d4566
gaoooon <gaon9749@gmail.com>
2024-11-24 22:35:26 +0900

    Feat : Button, Input, Wrapper test 파일 추가

---
+++ src/components/__test__/Wrapper.test.tsx
@@ -0,0 +1,40 @@
+import { Wrapper } from '@/components';
+import { renderWithEmotion } from '@/utils';
+
+describe('Wrapper Component', () => {
+  test('Success Case', () => {
+    const titleText = 'id를 입력해주세요';
+    const successMessageText = 'id 입력을 확인했습니다';
+
+    const { container } = renderWithEmotion(
+      <Wrapper title={titleText} successMessage={successMessageText}>
+        <input />
+      </Wrapper>
+    );
+
+    expect(container.firstChild?.childNodes[0].textContent).toBe(titleText);
+
+    expect(container.firstChild?.childNodes[2].textContent).toBe(
+      successMessageText
+    );
+  });
+
+  test('Error Case', () => {
+    const titleText = 'password를 입력해주세요';
+    const errorMessageText = 'password를 입력을 확인하지 못했습니다';
+
+    const { container } = renderWithEmotion(
+      <Wrapper title={titleText} errorMessage={errorMessageText}>
+        <input />
+      </Wrapper>
+    );
+
+    expect(container.firstChild?.childNodes[0].textContent).toBe(titleText);
+
+    expect(container.firstChild?.childNodes[2].textContent).toBe(
+      errorMessageText
+    );
+
+    expect(container.firstChild).toBeInTheDocument();
+  });
+});
