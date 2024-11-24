6839aed0fc03f0b81ad0407064511000b74558fd
gaoooon <gaon9749@gmail.com>
2024-11-24 22:34:40 +0900

    fix : export 수정

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,3 @@
-export * from './FormItem';
 export { default as Button } from './Button';
+export { default as Input } from './FormItem/Input';
+export { default as Wrapper } from './FormItem/Wrapper';
