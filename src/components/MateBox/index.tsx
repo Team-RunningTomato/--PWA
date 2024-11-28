b6cb93a99902eac7140af41f59510c39325db542
gjaegyun <jaegyungim06@gmail.com>
2024-11-28 18:18:51 +0900

    feat: link 태그 추가

--- src/components/MateBox/index.tsx
+++ src/components/MateBox/index.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import Link from 'next/link';
+
 import { LocationIcon } from '@/assets';
 
 import * as S from './style';
@@ -14,23 +16,25 @@ interface MateBoxProps {
 
 const MateBox = ({ distance, location, title, date, time }: MateBoxProps) => {
   return (
-    <S.Wrapper>
-      <S.Container>
-        <S.MainBox>
-          <S.RunningInfoBox>
-            <S.Distance>{distance}km</S.Distance>
-            <S.LocationBox>
-              <LocationIcon />
-              <S.LocationText>{location}</S.LocationText>
-            </S.LocationBox>
-          </S.RunningInfoBox>
-          <S.Title>{title}</S.Title>
-        </S.MainBox>
-        <S.TimeText>
-          {date} / {time}
-        </S.TimeText>
-      </S.Container>
-    </S.Wrapper>
+    <Link href={''}>
+      <S.Wrapper>
+        <S.Container>
+          <S.MainBox>
+            <S.RunningInfoBox>
+              <S.Distance>{distance}km</S.Distance>
+              <S.LocationBox>
+                <LocationIcon />
+                <S.LocationText>{location}</S.LocationText>
+              </S.LocationBox>
+            </S.RunningInfoBox>
+            <S.Title>{title}</S.Title>
+          </S.MainBox>
+          <S.TimeText>
+            {date} / {time}
+          </S.TimeText>
+        </S.Container>
+      </S.Wrapper>
+    </Link>
   );
 };
 
