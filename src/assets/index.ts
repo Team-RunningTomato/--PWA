236cf6cf1cac6c75dadbad7854f0e05cf588a9a2
gjaegyun <jaegyungim06@gmail.com>
2024-12-18 09:14:20 +0900

    feat: icon 추가

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,3 +1,7 @@
 export { default as NaverLogo } from './NaverLogo';
 export { default as LocationIcon } from './LocationIcon';
 export { default as KebabMenuIcon } from './KebabMenuIcon';
+export { default as RunningHallaeIcon } from './RunningHallaeIcon';
+export { default as RunningHallaeText } from './RunningHallaeText';
+export { default as SearchIcon } from './SearchIcon';
+export { default as AlarmIcon } from './AlarmIcon';
