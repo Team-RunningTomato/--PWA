d07c40639b432805289cb5ad5179d5c939d663b7
gjaegyun <jaegyungim06@gmail.com>
2024-12-18 09:14:46 +0900

    feat: topBar 컴포넌트 제작

---
+++ src/components/TopBar/index.tsx
@@ -0,0 +1,33 @@
+'use client';
+
+import {
+  AlarmIcon,
+  RunningHallaeIcon,
+  RunningHallaeText,
+  SearchIcon,
+} from '@/assets';
+
+import * as S from './style';
+
+const TopBar = () => {
+  return (
+    <S.Wrapper>
+      <S.LogoContainer>
+        <S.LogoBox>
+          <RunningHallaeIcon />
+        </S.LogoBox>
+        <RunningHallaeText />
+      </S.LogoContainer>
+      <S.IconContainer>
+        <S.Button>
+          <SearchIcon />
+        </S.Button>
+        <S.Button>
+          <AlarmIcon />
+        </S.Button>
+      </S.IconContainer>
+    </S.Wrapper>
+  );
+};
+
+export default TopBar;
