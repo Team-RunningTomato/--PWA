773d2880066344742b5047d4b2d61d244b294dab
gjaegyun <jaegyungim06@gmail.com>
2024-12-18 09:37:22 +0900

    feat: icon 추가

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,3 +1,7 @@
 export { default as NaverLogo } from './NaverLogo';
 export { default as LocationIcon } from './LocationIcon';
 export { default as KebabMenuIcon } from './KebabMenuIcon';
+export { default as HomeIcon } from './HomeIcon';
+export { default as WingIcon } from './WingIcon';
+export { default as JoinIcon } from './JoinIcon';
+export { default as ProfileIcon } from './ProfileIcon';
