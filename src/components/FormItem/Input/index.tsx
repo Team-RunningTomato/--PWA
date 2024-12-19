e5bc39d4a072442b60933d4040e0eba10bd3f065
gaoooon <gaon9749@gmail.com>
2024-12-19 21:15:54 +0900

    fix : input 컴포넌트 unit prop추가, style 수정

--- src/components/FormItem/Input/index.tsx
+++ src/components/FormItem/Input/index.tsx
@@ -10,21 +10,25 @@ interface Props extends React.InputHTMLAttributes<HTMLInputElement> {
   inputTitle?: string;
   errorMessage?: string;
   successMessage?: string;
+  unit?: string;
 }
 
 const Input = forwardRef<HTMLInputElement, Props>(
-  ({ inputTitle, errorMessage, successMessage, ...attributes }, ref) => (
+  ({ inputTitle, errorMessage, successMessage, unit, ...attributes }, ref) => (
     <Wrapper
       title={inputTitle}
       errorMessage={errorMessage}
       successMessage={successMessage}
     >
-      <S.CustomInput
-        ref={ref}
-        isError={!!errorMessage}
-        isSuccess={!!successMessage}
-        {...attributes}
-      />
+      <S.Container>
+        <S.CustomInput
+          ref={ref}
+          isError={!!errorMessage}
+          isSuccess={!!successMessage}
+          {...attributes}
+        />
+        {!!unit && <S.Unit>{unit}</S.Unit>}
+      </S.Container>
     </Wrapper>
   )
 );
