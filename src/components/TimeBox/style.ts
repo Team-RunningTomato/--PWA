bb4e99ffa6c9a0f9aa7458c4dae428ce7f891a77
gjaegyun <jaegyungim06@gmail.com>
2024-12-19 23:07:13 +0900

    feat: time 조건식 생성 및 디자인 완성

--- src/components/TimeBox/style.ts
+++ src/components/TimeBox/style.ts
@@ -6,3 +6,52 @@ export const Wrapper = styled.div`
   justify-content: center;
   gap: 1rem;
 `;
+
+export const TimeText = styled.h2`
+  ${({ theme }) => theme.typo.body1B};
+  color: ${({ theme }) => theme.color.black};
+`;
+
+export const TimeBox = styled.div`
+  display: flex;
+  padding: 0rem 0.5rem;
+  gap: 5.5rem;
+`;
+
+export const AMPMBox = styled.div`
+  display: flex;
+  gap: 1.5rem;
+`;
+
+export const AMPMText = styled.p<{ selected: boolean }>`
+  ${({ theme }) => theme.typo.body1R};
+  color: ${({ theme, selected }) =>
+    selected ? theme.color.black : theme.color.gray[500]};
+  cursor: pointer;
+`;
+
+export const MinuteBox = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 1.5rem;
+`;
+
+export const MinuteInput = styled.input`
+  ${({ theme }) => theme.typo.body1B};
+  color: ${({ theme }) => theme.color.black};
+  border: none;
+  width: 1.75rem;
+
+  ::placeholder {
+    color: ${({ theme }) => theme.color.gray[500]};
+  }
+
+  :focus {
+    outline: none;
+  }
+`;
+
+export const MiddleText = styled.p`
+  ${({ theme }) => theme.typo.body2B};
+  color: ${({ theme }) => theme.color.black};
+`;
