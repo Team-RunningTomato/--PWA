7fd7292c10736e2a407f5fe28d0923c7e02fcbee
gjaegyun <jaegyungim06@gmail.com>
2024-12-19 19:54:50 +0900

    feat: icon 추가

---
+++ src/assets/LeftArrow.tsx
@@ -0,0 +1,19 @@
+const LeftArrow = () => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    width='2.25rem'
+    height='2.25rem'
+    viewBox='0 0 36 36'
+    fill='none'
+  >
+    <path
+      d='M22.5 9L13.5 18L22.5 27'
+      stroke='#121212'
+      strokeWidth='1.2'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+    />
+  </svg>
+);
+
+export default LeftArrow;
