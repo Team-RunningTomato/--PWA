a22dfcab13d2774010d47fbe1ce3c1e04e2d90d9
gjaegyun <jaegyungim06@gmail.com>
2024-12-19 09:07:28 +0900

    feat: sort period 적용

--- src/components/FilterModal/index.tsx
+++ src/components/FilterModal/index.tsx
@@ -1,24 +1,26 @@
 'use client';
 
 import useFilterModalStore from '@/stores/filterModalStore';
+import usePeriodStore from '@/stores/periodStore';
 
 import * as S from './style';
 
 const FilterModal = () => {
   const { closeFilterModal } = useFilterModalStore();
+  const { setSortPeriod } = usePeriodStore();
 
   return (
     <S.Backdrop>
-      <S.Wrapper onClick={(e) => e.stopPropagation()}>
-        <S.Options>
+      <S.Wrapper onClick={closeFilterModal}>
+        <S.Options onClick={() => setSortPeriod('latest')}>
           <S.Text>최신순</S.Text>
         </S.Options>
 
-        <S.Options>
+        <S.Options onClick={() => setSortPeriod('oldest')}>
           <S.Text>오래된순</S.Text>
         </S.Options>
 
-        <S.Options onClick={closeFilterModal}>
+        <S.Options>
           <S.Text isCancle={true}>취소</S.Text>
         </S.Options>
       </S.Wrapper>
