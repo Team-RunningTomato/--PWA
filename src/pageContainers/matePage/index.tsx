90d0042059c36a246376bd04a927a1e67cae7649
gjaegyun <jaegyungim06@gmail.com>
2024-12-20 10:53:25 +0900

    feat: 한개 날짜도 data에 저장

--- src/pageContainers/matePage/index.tsx
+++ src/pageContainers/matePage/index.tsx
@@ -2,12 +2,34 @@
 
 import { DetailLocationIcon } from '@/assets';
 import { Button, Input, MateBottomSheet, NavigationHeader } from '@/components';
-import { useMateSheetStore } from '@/stores';
+import { useDateStore, useMateSheetStore } from '@/stores';
+
+import { useState } from 'react';
 
 import * as S from './style';
 
 const MatePage = () => {
   const { isMateSheetOpen, openMateSheet } = useMateSheetStore();
+  const { selectedDates } = useDateStore();
+
+  const [isClicked, setIsClicked] = useState<boolean>(false);
+
+  const formatDate = (month: number, date: number) => `${month}월 ${date}일`;
+
+  const renderDateRange = () => {
+    if (selectedDates.length === 1) {
+      const { month, date } = selectedDates[0];
+      return formatDate(month, date);
+    } else if (selectedDates.length > 1) {
+      const { month: startMonth, date: startDate } = selectedDates[0];
+      const { month: endMonth, date: endDate } =
+        selectedDates[selectedDates.length - 1];
+      return `${formatDate(startMonth, startDate)} - ${formatDate(endMonth, endDate)}`;
+    }
+    return '만날 시간을 입력해주세요.';
+  };
+
+  console.log(renderDateRange());
 
   return (
     <>
@@ -29,15 +51,10 @@ const MatePage = () => {
         />
         <S.ButtonContainer>
           <S.Title>약속</S.Title>
-          <S.Button onClick={openMateSheet}>만날 시간을 입력해주세요.</S.Button>
+          <S.Button onClick={openMateSheet}>{renderDateRange()}</S.Button>
         </S.ButtonContainer>
         <S.ButtonWrapper>
-          <Button
-            title={'확인'}
-            onClick={function (): void {
-              throw new Error('Function not implemented.');
-            }}
-          />
+          <Button title={'확인'} onClick={() => setIsClicked(true)} />
         </S.ButtonWrapper>
       </S.Wrapper>
       {isMateSheetOpen && <MateBottomSheet />}
