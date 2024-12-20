f0c8166dacdb45393b734865398f6e0d8db64333
gjaegyun <jaegyungim06@gmail.com>
2024-12-20 10:15:42 +0900

    feat: import 방식 변경

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -2,4 +2,4 @@ export { default as bottomSheetStore } from './bottomSheetStore';
 export { default as periodStore } from './periodStore';
 export { default as useMateSheetStore } from './mateSheetStore';
 export { default as dateStore } from './dateStore';
-export { default as timeStore } from './timeStore';
+export { default as useTimeStore } from './timeStore';
