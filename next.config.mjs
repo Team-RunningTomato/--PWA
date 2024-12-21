8ce062771aa13d8b9034277602ef0f1f3908b38d
gaoooon <gaon9749@gmail.com>
2024-12-22 01:03:01 +0900

    fix : rewrites 주소 수정

--- next.config.mjs
+++ next.config.mjs
@@ -2,15 +2,15 @@ import withPWAInit from '@ducanh2912/next-pwa';
 
 const withPWA = withPWAInit({
   dest: 'public',
-  register: true,
+  register: false,
   skipWaiting: true,
 });
 
 export default withPWA({
   rewrites: async () => [
     {
-      source: '/api:path*',
-      destination: `${process.env.NEXT_PUBLIC_API_BASE_URL}:path*`,
+      source: '/api/:path*',
+      destination: `${process.env.NEXT_PUBLIC_API_BASE_URL}/:path*`,
     },
   ],
 });
