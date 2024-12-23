133be5d2d97311b1562952e5736b4d338eee68bb
gjaegyun <jaegyungim06@gmail.com>
2024-12-23 10:25:25 +0900

    feat: mate page schema 연결

--- src/pageContainers/mate/index.tsx
+++ src/pageContainers/mate/index.tsx
@@ -1,17 +1,36 @@
 'use client';
 
-import { DetailLocationIcon } from '@/assets';
-import { Button, Input, MateBottomSheet, NavigationHeader } from '@/components';
-import { useDateStore, useMateSheetStore, useTimeStore } from '@/stores';
+import { zodResolver } from '@hookform/resolvers/zod';
 
-import { useState } from 'react';
+import { DetailLocationIcon } from '@/assets';
+import { Input, MateBottomSheet, NavigationHeader } from '@/components';
+import { mateInfoSchema } from '@/schemas';
+import { useDateStore, useMateSheetStore, useTimeStore } from '@/stores';
+import { MateInfoFormType } from '@/types';
+
+import { useEffect, useState } from 'react';
+import { Address, useDaumPostcodePopup } from 'react-daum-postcode';
+import { SubmitHandler, useForm } from 'react-hook-form';
 
 import * as S from './style';
 
 const MatePage = () => {
+  const {
+    register,
+    formState: {
+      errors: { title, location, address, startLocation, distance, date },
+    },
+    setValue,
+    handleSubmit,
+  } = useForm<MateInfoFormType>({
+    resolver: zodResolver(mateInfoSchema),
+  });
+
   const { isMateSheetOpen, openMateSheet } = useMateSheetStore();
   const { selectedDates } = useDateStore();
   const { AMPM, hour, minute } = useTimeStore();
+
+  const daumPostCode = useDaumPostcodePopup();
 
   const [isClicked, setIsClicked] = useState<boolean>(false);
 
@@ -30,19 +49,30 @@ const MatePage = () => {
     return `${period} ${formattedHour}시 ${formattedMinute}분`;
   };
 
+  useEffect(() => {
+    if (isClicked && !isNull && selectedDates.length > 0) {
+      renderDateRange();
+    }
+  }, [isClicked, selectedDates, AMPM, hour, minute]);
+
   const renderDateRange = () => {
+    let dateRange = '';
+
     if (isClicked === true && isNull === false) {
       if (selectedDates.length === 1) {
         const { month, date } = selectedDates[0];
-        return `${formatDate(month, date)} ${formatTime(AMPM, hour, minute)}`;
+        dateRange = `${formatDate(month, date)} ${formatTime(AMPM, hour, minute)}`;
       } else if (selectedDates.length > 1) {
         const { month: startMonth, date: startDate } = selectedDates[0];
         const { month: endMonth, date: endDate } =
           selectedDates[selectedDates.length - 1];
-        return `${formatDate(startMonth, startDate)} - ${formatDate(endMonth, endDate)}`;
+        dateRange = `${formatDate(startMonth, startDate)} - ${formatDate(endMonth, endDate)} / ${formatTime(AMPM, hour, minute)}`;
       }
     }
-    return '만날 시간을 입력해주세요.';
+
+    setValue('date', dateRange);
+
+    return dateRange;
   };
 
   const handleBottomSheet = () => {
@@ -56,35 +86,118 @@ const MatePage = () => {
     }
   };
 
+  const handleDaumPostCodePopupComplete = ({ address, zonecode }: Address) => {
+    setValue('location', `${address} ${zonecode}`);
+  };
+
+  const handleLocationBtnClick = () =>
+    daumPostCode({
+      popupTitle: 'RUNNING HALLAE',
+      onComplete: handleDaumPostCodePopupComplete,
+    });
+
+  const handleStartDaumPostCodePopupComplete = ({
+    address,
+    zonecode,
+  }: Address) => {
+    setValue('startLocation', `${address} ${zonecode}`);
+  };
+
+  const handleStartLocationBtnClick = () =>
+    daumPostCode({
+      popupTitle: 'RUNNING HALLAE',
+      onComplete: handleStartDaumPostCodePopupComplete,
+    });
+
+  const handleFormSubmit: SubmitHandler<MateInfoFormType> = ({
+    title,
+    location,
+    address,
+    distance,
+    date,
+  }) => {
+    const body = {
+      title: title,
+      location: location,
+      address: address,
+      distance: distance,
+      date: date,
+    };
+    console.log(body);
+  };
+
   return (
     <>
       <NavigationHeader navigateUrl={'/'} title={'런닝 모집하기'} />
-      <S.Wrapper>
-        <Input inputTitle='제목' placeholder='제목을 입력해주세요.' />
-        <S.InputContainer>
-          <Input
-            inputTitle='장소'
-            placeholder='위치를 알려주세요.'
-            icon={<DetailLocationIcon />}
-          />
-          <Input placeholder='상세주소를 알려주세요.' />
-        </S.InputContainer>
+      <S.Wrapper onSubmit={handleSubmit(handleFormSubmit)} noValidate>
         <Input
-          inputTitle='거리'
-          placeholder='런닝 거리 입력해주세요.'
-          unit={'km'}
+          {...register('title')}
+          inputTitle='제목'
+          placeholder='제목을 입력해주세요.'
+          errorMessage={title?.message}
         />
+        <S.InputContainer>
+          <S.ButtonContainer>
+            <S.Title>장소</S.Title>
+            <S.InputButton onClick={handleLocationBtnClick} type='button'>
+              <Input
+                {...register('location')}
+                placeholder='위치를 알려주세요.'
+                icon={<DetailLocationIcon />}
+                style={{
+                  pointerEvents: 'none',
+                }}
+                errorMessage={location?.message}
+              />
+            </S.InputButton>
+            <Input
+              {...register('address')}
+              placeholder='상세주소를 알려주세요.'
+              errorMessage={address?.message}
+            />
+          </S.ButtonContainer>
+        </S.InputContainer>
+        <S.InputContainer>
+          <S.ButtonContainer>
+            <S.Title>거리</S.Title>
+            <S.InputButton onClick={handleStartLocationBtnClick} type='button'>
+              <Input
+                {...register('startLocation')}
+                placeholder='출발 위치를 알려주세요.'
+                icon={<DetailLocationIcon />}
+                style={{
+                  pointerEvents: 'none',
+                }}
+                errorMessage={startLocation?.message}
+              />
+            </S.InputButton>
+            <Input
+              {...register('distance', {
+                valueAsNumber: true,
+              })}
+              placeholder='런닝 거리를 입력해주세요.'
+              unit={'km'}
+              errorMessage={distance?.message}
+            />
+          </S.ButtonContainer>
+        </S.InputContainer>
+
         <S.ButtonContainer>
           <S.Title>약속</S.Title>
-          <S.Button onClick={openMateSheet}>{renderDateRange()}</S.Button>
+          <S.InputButton onClick={openMateSheet} type='button'>
+            <Input
+              {...register('date')}
+              placeholder='만날 시간을 입력해주세요'
+              style={{
+                pointerEvents: 'none',
+              }}
+              errorMessage={date?.message}
+              readOnly
+            />
+          </S.InputButton>
         </S.ButtonContainer>
         <S.ButtonWrapper>
-          <Button
-            title={'확인'}
-            onClick={function (): void {
-              throw new Error('Function not implemented.');
-            }}
-          />
+          <S.ConfirmButton type='submit'>확인</S.ConfirmButton>
         </S.ButtonWrapper>
       </S.Wrapper>
       {isMateSheetOpen && <MateBottomSheet onClick={handleBottomSheet} />}
