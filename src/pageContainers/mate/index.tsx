ab2654192029604e4d5bbbf73212618dcf5cf09c
gjaegyun <jaegyungim06@gmail.com>
2024-12-26 08:46:13 +0900

    feat: app mate page 추가

--- src/pageContainers/mate/index.tsx
+++ src/pageContainers/mate/index.tsx
@@ -18,7 +18,7 @@ import { SubmitHandler, useForm } from 'react-hook-form';
 import * as S from './style';
 
 const MatePage = () => {
-  // const geoCoder = new window.kakao.maps.services.Geocoder();
+  const geoCoder = new window.kakao.maps.services.Geocoder();
   // const [geoCoder, setGeoCoder] = useState<any>(null);
 
   // useEffect(() => {
@@ -119,25 +119,25 @@ const MatePage = () => {
   }: Address) => {
     setValue('startLocation', `${address} ${zonecode}`);
 
-    // const getAddressCoords = (address: string) => {
-    //   if (!geoCoder) {
-    //     console.error('GeoCoder가 초기화되지 않았습니다.');
-    //     return;
-    //   }
+    const getAddressCoords = (address: string) => {
+      if (!geoCoder) {
+        console.error('GeoCoder가 초기화되지 않았습니다.');
+        return;
+      }
 
-    //   return new Promise((resolve, reject) => {
-    //     geoCoder.addressSearch(address, (result: any, status: any) => {
-    //       if (status === kakao.maps.services.Status.OK) {
-    //         const coords = new kakao.maps.LatLng(result[0].y, result[0].x);
-    //         resolve(coords);
-    //       } else {
-    //         reject(status);
-    //       }
-    //     });
-    //   });
-    // };
+      return new Promise((resolve, reject) => {
+        geoCoder.addressSearch(address, (result: any, status: any) => {
+          if (status === kakao.maps.services.Status.OK) {
+            const coords = new kakao.maps.LatLng(result[0].y, result[0].x);
+            resolve(coords);
+          } else {
+            reject(status);
+          }
+        });
+      });
+    };
 
-    // getAddressCoords(address);
+    getAddressCoords(address);
   };
 
   const handleStartLocationBtnClick = () =>
