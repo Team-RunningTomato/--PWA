4afc1fc6ae4840ed4c7f3ea702269808b6f5e7ef
gjaegyun <jaegyungim06@gmail.com>
2024-12-27 14:20:51 +0900

    feat: useGetMyMateInfo.ts 제작

---
+++ src/hooks/apis/user/useGetMyMateInfo.ts
@@ -0,0 +1,18 @@
+import { AxiosError } from 'axios';
+
+import { get, userQueryKeys, userUrls } from '@/libs';
+import { MyMateInfoType } from '@/types';
+
+import { UseQueryOptions, useQuery } from '@tanstack/react-query';
+
+export const useGetMyMateInfo = (
+  options?: Omit<
+    UseQueryOptions<MyMateInfoType, AxiosError>,
+    'queryKey' | 'queryFn'
+  >
+) =>
+  useQuery({
+    queryKey: userQueryKeys.getMyMateInfo(),
+    queryFn: () => get<MyMateInfoType>(userUrls.getMyMateInfo()),
+    ...options,
+  });
--- src/libs/api/queryKeys.ts
+++ src/libs/api/queryKeys.ts
@@ -2,6 +2,7 @@ export const userQueryKeys = {
   patchMyBodyInfo: () => ['body', 'height', 'weight'],
   getMyInfo: () => ['user', 'get', 'info'],
   getMyRunningApplication: () => ['running', 'application', 'my'],
+  getMyMateInfo: () => ['mate', 'info', 'my'],
 } as const;
 
 export const meetingQueryKeys = {
