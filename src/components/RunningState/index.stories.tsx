83eac4803a267b396a0f99496794d8a086ee3e3b
gjaegyun <jaegyungim06@gmail.com>
2024-12-29 00:38:18 +0900

    feat: 필드 네이밍 변경에 따른 변경사항

--- src/components/RunningState/index.stories.tsx
+++ src/components/RunningState/index.stories.tsx
@@ -15,8 +15,8 @@ const meta: Meta<typeof RunningState> = {
     date: '11월 27일 수요일 / 오전 02시 10분',
     level: 1,
     totalDistance: 15,
-    bestDistance: 10,
-    worstDistance: 1,
+    longestDistance: 10,
+    shortestDistance: 1,
     todayRunning: [
       '광주소프트웨어마이스터고에서 런닝 모집',
       '광주소프트웨어마이스터고에서 런닝 모집',
