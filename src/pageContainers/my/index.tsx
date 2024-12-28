76bcf3d2dfd9df95ab4ac870addc62a302bed452
gjaegyun <jaegyungim06@gmail.com>
2024-12-29 00:30:32 +0900

    feat: 기본값 상수로 치환

--- src/pageContainers/my/index.tsx
+++ src/pageContainers/my/index.tsx
@@ -19,20 +19,23 @@ const MyPage = () => {
     level,
   } = runningUser || {};
 
+  const defaultString = '';
+  const defaultNumber = 0;
+
   return (
     <S.Wrapper>
       <S.Container>
         <Profile
-          name={name ?? ''}
-          level={level ?? 0}
-          kg={weight ?? ''}
-          height={height ?? ''}
+          name={name ?? defaultString}
+          level={level ?? defaultNumber}
+          kg={weight ?? defaultString}
+          height={height ?? defaultString}
         />
         <RunningLevelBox
-          totalDistance={totalDistance ?? 0}
-          longestDistance={longestDistance ?? 0}
-          shortestDistance={shortestDistance ?? 0}
-          percent={levelPercentage ?? 0}
+          totalDistance={totalDistance ?? defaultNumber}
+          longestDistance={longestDistance ?? defaultNumber}
+          shortestDistance={shortestDistance ?? defaultNumber}
+          percent={levelPercentage ?? defaultNumber}
         />
         <S.MateWrapper>
           <S.Title>내가 모집한 런닝</S.Title>
